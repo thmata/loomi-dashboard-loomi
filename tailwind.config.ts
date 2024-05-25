a8a0f4e28b5863b7d9e9725059329ebfede608aa
Thiago Mata <taiagomata@gmail.com>
2024-05-25 10:40:37 -0300

    feat: visual structure login screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+        nunito: ['Nunito Sans', 'sans-serif'],
+      }
     },
   },
   plugins: [],
