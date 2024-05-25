5deac8b07669f74912dd275e06d1ba3399734895
Thiago Mata <taiagomata@gmail.com>
2024-05-25 16:17:53 -0300

    Feat: created dashboard page

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+    title: "Dashboard - Início"
+}
+
+export default function DashboardLayout({ children }: any) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quis saepe architecto quam dicta voluptates itaque consequatur, tempora recusandae distinctio eos ipsa, dolorem quos maiores, aliquid error dolore aliquam iste.
+        </div>
+    );
+};
+
+export default Dashboard;
